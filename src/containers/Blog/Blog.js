fb8d60f9af1d729c6b2c71b1679eee2d13873759
Sandeep <sandeeep021@gmail.com>
2019-06-28 19:34:54 +0530

    called api to get post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,32 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts : []
+    }
+    //Calling api
+    componentDidMount(){ 
+
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then( response => {
+            this.setState({ posts:response.data });
+        });
+    }
     render () {
+        //Set data for to show as list
+        const posts = this.state.posts.map( post => {
+            return (<Post title={post.title} 
+                          key={post.id}
+                          info={post.body}
+                   />);
+        })
         return (
             <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                <section className="Posts">77777777777777777
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
